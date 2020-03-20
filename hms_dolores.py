bb1822b7d3c758d276ae033dab3aa0ab45c0fd16
sansLiberte <adrian.yx.tan@gmail.com>
2020-03-21 01:11:49 +0800

    Initial file creation

---
+++ hms_dolores.py
@@ -0,0 +1,6 @@
+#HMS Dolores
+#Board game implementation of HMS Dolores
+
+
+#Deck consists of seven different types of goods, each of which has seven 1-value cards,
+#two 2-value cards and one 3-value card.
